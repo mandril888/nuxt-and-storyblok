5a5493b76a1cd0e03636476c965d7ff8dfb5d1f6
Arturo Rubio <arubio@seocom.agency>
2023-06-04 18:07:00 +0200

    create plugins and configs

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/components'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -38,6 +39,13 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    [
+      'storyblok-nuxt',
+      {
+        accessToken: 'QBVBONn6kpkDPMcqu5UtAAtt',
+        cacheProvider: 'memory'
+      }
+    ],
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
