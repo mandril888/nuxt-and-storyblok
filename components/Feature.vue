2c6ab5d4be8aed2eae024821081cefc2a10d627c
Arturo Rubio <arubio@seocom.agency>
2023-06-05 16:47:15 +0200

    add icon and description to Feature component

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,8 +1,8 @@
 <template>
-    <div
-      v-editable="blok"
-      class="py-2">
+    <div v-editable="blok" class="py-2 text-center">
+      <img class="mx-auto w-24" :src="blok.icon">
       <h1 class="text-lg">{{ blok.name }}</h1>
+      <p class="text-gray-600">{{ blok.description }}</p>
     </div>
   </template>
    
